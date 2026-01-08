d7f8fc742b34b9a392bd494b0b9ab5b86fdb11b1
Omar <elmanssouriomar@gmail.com>
2026-01-08 17:24:22 +0100

    Add proper HTTP status codes

--- src/main/java/com/omar/demoapi/controller/HelloController.java
+++ src/main/java/com/omar/demoapi/controller/HelloController.java
@@ -1,26 +1,20 @@
 package com.omar.demoapi.controller;
 
 import com.omar.demoapi.dto.HelloRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/hello")
 public class HelloController {
 
-    @GetMapping
-    public String sayHello() {
-        return "Hello!";
-    }
+//    @GetMapping
+//    public String sayHello() { return "Hello!"; }
 
     @GetMapping("/{name}")
     public String helloWithName(@PathVariable String name) {
         return "Hello " + name;
     }
-
-//    @GetMapping("/greet")
-//    public String helloWithParam(@RequestParam String name) {
-//        return "Hello " + name;
-//    }
 
     @GetMapping("/greet")
     public String greetWithParam(
@@ -30,9 +24,28 @@ public class HelloController {
     }
 
 
+//    @PostMapping
+//    public String helloPost(@RequestBody HelloRequest request) {
+//        return "Hello " + request.getName() + ", age " + request.getAge();
+//    }
+
+    // Examples of using ResponseEntity
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello from Spring Boot API!");
+    }
+
     @PostMapping
-    public String helloPost(@RequestBody HelloRequest request) {
-        return "Hello " + request.getName() + ", age " + request.getAge();
+    public ResponseEntity<String> helloPost(@RequestBody HelloRequest request) {
+
+        if(request.getName() == null || request.getName().isBlank()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Name is required");
+        }
+
+        String response = "Hello " + request.getName() + ", age " + request.getAge();
+        return ResponseEntity.status(201).body(response);
     }
 
 }
