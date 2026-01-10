045b6ccd9e87b33f715086515555e8f1dc2ccd79
Omar <elmanssouriomar@gmail.com>
2026-01-10 16:50:59 +0100

    feat: migrate user persistence to PostgreSQL using Spring Data JPA

--- src/main/java/com/omar/demoapi/entity/User.java
+++ src/main/java/com/omar/demoapi/entity/User.java
@@ -1,10 +1,22 @@
 package com.omar.demoapi.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
 
     public User() {}
--- src/main/java/com/omar/demoapi/repository/UserRepository.java
+++ src/main/java/com/omar/demoapi/repository/UserRepository.java
@@ -1,17 +1,13 @@
 package com.omar.demoapi.repository;
 
 import com.omar.demoapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository {
 
-    List<User> findAll();
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findById(Long id);
+    Optional<User> findByEmail(String email);
 
-    User save(User user);
-
-    void deleteById(Long id);
 }
